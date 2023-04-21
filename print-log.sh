371b1cefb6208bc08c251538cd653b776670c39a
rachaelpai <komimi.p@gmail.com>
2023-04-21 14:59:43 +0800

    make print-log stand-alone

--- print-log.sh
+++ print-log.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+# print-log.sh
 # shellcheck source=/dev/null
+set -x
 source utils.sh
-source env-artifact.sh
+mango_simulation_artifact_bucket="$ARTIFACT_BUCKET/$BUILDKITE_PIPELINE_ID/$BUILDKITE_BUILD_ID/$BUILDKITE_JOB_ID"
 echo ---- stage: print log ----
 if [[ "$PRINT_LOG" == "true" ]];then
-    download_file "gs://$MANGO_SIMULATION_ARTIFACT_BUCKET" start-dos-test.nohup ./
+    download_file "gs://$mango_simulation_artifact_bucket" start-dos-test.nohup ./
 fi
 if [[ -f  "start-dos-test.nohup" ]];then
     cat start-dos-test.nohup
+else 
+    echo "no start-dos-test.nohup found"
 fi
