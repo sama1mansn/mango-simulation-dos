e9d3af92e45bb9020a860781f1dca1d2da26a697
rachaelpai <komimi.p@gmail.com>
2023-04-21 15:44:10 +0800

    prevent download to fail

--- print-log.sh
+++ print-log.sh
@@ -6,7 +6,7 @@ source utils.sh
 mango_simulation_artifact_bucket="$ARTIFACT_BUCKET/$BUILDKITE_PIPELINE_ID/$BUILDKITE_BUILD_ID/$BUILDKITE_JOB_ID"
 echo ---- stage: print log ----
 if [[ "$PRINT_LOG" == "true" ]];then
-    download_file "gs://$mango_simulation_artifact_bucket" start-dos-test.nohup ./
+    ret_download_log=$(download_file "gs://$mango_simulation_artifact_bucket" start-dos-test.nohup ./) || true
 fi
 if [[ -f  "start-dos-test.nohup" ]];then
     cat start-dos-test.nohup
