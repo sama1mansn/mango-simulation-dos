838ed43697edda590a9e61483f524d3ecc87fe60
rachaelpai <komimi.p@gmail.com>
2023-04-21 22:34:55 +0800

    fix print-log.sh error

--- print-log.sh
+++ print-log.sh
@@ -9,9 +9,9 @@ if [[ "$PRINT_LOG" == "true" ]];then
     ret_download_log=$(download_file "gs://$ARTIFACT_BUCKET/$BUILDKITE_PIPELINE_ID/$BUILDKITE_BUILD_ID" start-dos-test-1.nohup ./) || true
 fi
 sleep 3
-if [[ -f  "start-dos-test1.nohup" ]];then
-    cat start-dos-test1.nohup
+if [[ -f  "start-dos-test-1.nohup" ]];then
+    cat start-dos-test-1.nohup
 else 
-    echo "no start-dos-test1.nohup found"
+    echo "no start-dos-test-1.nohup found"
 fi
 exit 0
