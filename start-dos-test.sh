6871708fc87d282ba6bc8ec929b0151c451b4ec7
rachaelpai <komimi.p@gmail.com>
2023-05-04 12:17:11 +0800

    modify show solana-sys-tuner status

--- start-dos-test.sh
+++ start-dos-test.sh
@@ -62,7 +62,8 @@ export CLUSTER=$CLUSTER
 export RUST_LOG=$RUST_LOG
 export ENDPOINT_URL=$ENDPOINT
 ### check solana-sys-tuner status
-sudo systemctl status solana-sys-tuner || true
+sudo systemctl status solana-sys-tuner > sysstatus.out || true
+[[ -f "sysstatus.out" ]] && cat sysstatus.out
 ## Prepare Metrics Env
 configureMetrics
 ## Prepare Log Directory
